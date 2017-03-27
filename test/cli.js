725ed7e5817b5195b5c1447612bb03e2248f6a32
fand <fand.gmork@gmail.com>
2017-03-27 20:24:20 +0900

    Add tests for CLI with no options

--- test/cli.js
+++ test/cli.js
@@ -2,6 +2,25 @@ import test from 'ava';
 import fs from 'fs';
 import { execFileSync } from 'child_process';
 
+test('async-node', t => {
+  const actual = execFileSync(`${__dirname}/../bin/cli.js`, [`${__dirname}/input`]).toString();
+  t.is(actual, 'RESOLVED!\n', 'CLI executes JavaScript file Correctly');
+});
+
+test('async-node with invalid file', t => {
+  const error = t.throws(() => {
+    execFileSync(`${__dirname}/../bin/cli.js`, ['xxxxxx'], { stdio: ['pipe', 'pipe', 'pipe'] });
+  }, Error);
+  t.regex(error.stderr.toString(), /ENOENT/);
+});
+
+test('async-node with non-JavaScript file', t => {
+  const error = t.throws(() => {
+    execFileSync(`${__dirname}/../bin/cli.js`, [`${__dirname}/invalid`], { stdio: ['pipe', 'pipe', 'pipe'] });
+  }, Error);
+  t.regex(error.stderr.toString(), /ERROR/);
+});
+
 test('async-node -t', t => {
   const actual = execFileSync(`${__dirname}/../bin/cli.js`, ['-t', `${__dirname}/input`]).toString();
   const expected = fs.readFileSync(`${__dirname}/output`, 'utf8');
